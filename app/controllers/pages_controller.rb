e5554f80739b6572fd2617a512e7e083a31aefb2
_netyx <netyx0103@gmail.com>
2022-06-15 04:05:15 +0800

    homepage

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,5 @@
 class PagesController < ApplicationController
   def home
-    redirect_to 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', allow_other_host: true
   end
 
   def ee
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root "pages#home"
 
-  get "/ee",                to: "pages#ee"
-  get "/tok",               to: "pages#tok"
+  get "/ee",                to: "pages#ee",           as: :ee
+  get "/tok",               to: "pages#tok",          as: :tok
 
   get "/tok/orig/:object",  to: "pages#tok_original", as: :object
   get "/tok/image/:object", to: "pages#tok_image",    as: :object_image
