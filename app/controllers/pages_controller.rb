2a7515495e8f8d861e9d38b732fe8617221d1133
_netyx <netyx0103@gmail.com>
2022-06-15 03:02:32 +0800

    add tok, ee

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,10 @@ class PagesController < ApplicationController
   def home
     redirect_to 'https://www.youtube.com/watch?v=dQw4w9WgXcQ', allow_other_host: true
   end
+
+  def ee
+  end
+
+  def tok
+  end
 end
