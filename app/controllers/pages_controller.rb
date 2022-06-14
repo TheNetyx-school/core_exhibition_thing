73a25f171fb8f6fb6a44c80cf6364e87d7e5037b
_netyx <netyx0103@gmail.com>
2022-06-15 03:13:14 +0800

    add links to tok objects

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -8,4 +8,20 @@ class PagesController < ApplicationController
 
   def tok
   end
+
+  def tok_original
+    case(params[:object].to_i)
+    when 1
+      redirect_to "https://www.gnu.org/licenses/gpl-3.0.txt", allow_other_host: true
+    when 2
+      redirect_to "https://open.spotify.com/search/thomas%20the%20tank#login", allow_other_host: true
+    when 3
+      redirect_to "https://www.pinterest.com/pin/proverbs-267-nlt-getwisdomeveryday-for-the-lord-grants-wisdom-from-his-mouth-come-knowledge-and-understanding-he-grants-a-treas--278519558194960747/", allow_other_host: true
+    else
+      raise ActionController::RoutingError.new('Not Found')
+    end
+  end
+
+  def tok_image
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "pages#home"
 
-  get "/ee",  to: "pages#ee"
-  get "/tok", to: "pages#tok"
+  get "/ee",                to: "pages#ee"
+  get "/tok",               to: "pages#tok"
+
+  get "/tok/orig/:object",  to: "pages#tok_original"
+  get "/tok/image/:object", to: "pages#tok_image"
 end
